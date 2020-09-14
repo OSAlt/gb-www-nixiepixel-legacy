40f9d9a177647c4a8990c644ec65a9c94127cde5
Samir Faci <samir@esamir.com>
2020-09-13 22:09:14 -0700

    First pass at adding more content.

--- resources/views/components/sections/about.blade.php
+++ resources/views/components/sections/about.blade.php
@@ -1,27 +1,47 @@
 <section id="about" class="pb-4 pt-5">
     <h1 class="text-purple text-center mt-5">Who I Am & What I Do</h1>
-    <p class="text-purple text-center opacity-75">Nam vitae dolor fringilla, tempor massa quis, tincidunt velit.</p>
+    <p class="text-purple text-center opacity-75">Influencer, Educator, Producer, Speaker; a woman of many talents.</p>
     <hr class="w-25 mx-auto border-purple mb-5">
     <ul class="list-inline row justify-content-center justify-content-md-around text-light">
         <li class="list-inline-item text-center border border-light p-2 rounded col-4 col-md-2 mb-3 mb-md-0">
             <i class="fa fa-video mb-3 mb-md-4 text-black h2"></i>
-            <h4 class="text-blue">Lorem Ipsum</h4>
-            <p>Maecenas dapibus erat sit amet vulputate scelerisque.</p>
+            <h4 class="text-blue">Show Host</h4>
+            <p>Affiliate with <a href="https://en.wikipedia.org/wiki/G4_(American_TV_network)">G4TV</a> and TechTV (Discovery Channel)</p>
         </li>
         <li class="list-inline-item text-center border border-light p-2 rounded col-4 col-md-2 mb-3 mb-md-0 mr-0 mr-md-1">
             <i class="fa fa-microphone mb-3 mb-md-4 text-black h2"></i>
-            <h4 class="text-blue">Lorem Ipsum</h4>
-            <p>Curabitur orci leo, dapibus in euismod sed, feugiat ut lectus.</p>
+            <h4 class="text-blue">Speaker/Panelist</h4>
+            <p>
+                <ul>
+                    <li><a href="https://gdconf.com/">GDC</a> (Game Developer Conference)</li>
+                    <li> Zurich Game Festival.</li>
+                    <li><a href="https://www.paxsite.com/">Penny Arcade Expo</a><br /></li>
+                </ul>
+                
+            </p>
         </li>
         <li class="list-inline-item text-center border border-light p-2 rounded col-4 col-md-2">
             <i class="fa fa-linux mb-3 mb-md-4 text-black h2"></i>
-            <h4 class="text-blue">Lorem Ipsum</h4>
-            <p>Duis convallis mi non varius pretium.</p>
+            <h4 class="text-blue">Content</h4>
+            <p>Nixie has over a decade of experience in making complex ideas more approachable with her audience of over 300,000 subscribers and her videos focus on tech, gaming and more. </p>
         </li>
         <li class="list-inline-item text-center border border-light p-2 rounded col-4 col-md-2">
             <i class="fa fa-users mb-3 mb-md-4 text-black h2"></i>
-            <h4 class="text-blue">Lorem Ipsum</h4>
-            <p>Phasellus nibh mi, interdum a leo sed, egestas interdum elit.</p>
+            <h4 class="text-blue">Professional Sponsors</h4>
+            <p>Worked and collaborated with multiple high end sponsors including: <br>
+                AT&T, Dell, Ford, Kodak, Logitech
+            </p>
+        </li>
+        <li class="list-inline-item text-center border border-light p-2 rounded col-4 col-md-2">
+            <i class="fa fa-users mb-3 mb-md-4 text-black h2"></i>
+            <h4 class="text-blue">Founder / Entrepreneur </h4>
+            <p>
+                <ul>
+                    <li> Founded GeekBeacon, a digital community resource and mental health awareness center. <br/></li>
+               <li>Founded Equiti Games, An alternative distribution for games <br/> </li>
+               <li>Founded Geeks Abroad, a geeky travel show <br/></li>
+               <ul>
+            </p>
         </li>
     </ul>
 </section>
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -2,7 +2,7 @@
     <nav class="navbar navbar-expand-md navbar-dark border-transparent rounded-bottom">
 
         @component('components.social')
-            @slot('id', 'socialWeb')
+        @slot('id', 'socialWeb')
         @endcomponent
 
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
@@ -14,36 +14,43 @@
                 {{-- Check if route is the homepage --}}
                 @if(Request::path() == '/')
 
+                <li class="nav-item"><a href="#" class="nav-link text-light">Home</a></li>
+
                 <li class="nav-item"><a data-scroll href="#about" class="nav-link text-light">About</a></li>
 
                 <li class="nav-item"><a data-scroll href="#stats" class="nav-link text-light">Stats</a></li>
 
-                <li class="nav-item"><a data-scroll href="#media" class="nav-link text-light">Media</a></li>
+                <li class="nav-item"><a data-scroll href="#media" class="nav-link text-light">Instagram Feed</a></li>
 
                 <li class="nav-item"><a data-scroll href="#geekbeacon" class="nav-link text-light">GeekBeacon</a></li>
 
                 <li class="nav-item"><a data-scroll href="/#contact" class="nav-link text-light">Contact</a></li>
 
+
                 {{-- If Route isn't the homepage, then change hrefs and add a home option --}}
                 @else
-                    <li class="nav-item"><a data-scroll href="/#about" class="nav-link text-light">About</a></li>
 
-                    <li class="nav-item"><a data-scroll href="/#stats" class="nav-link text-light">Stats</a></li>
+                <li class="nav-item"><a href="/#" class="nav-link text-light">Home</a></li>
 
-                    <li class="nav-item"><a data-scroll href="/#media" class="nav-link text-light">Media</a></li>
+                <li class="nav-item"><a data-scroll href="/#about" class="nav-link text-light">About</a></li>
 
-                    <li class="nav-item"><a data-scroll href="/#geekbeacon" class="nav-link text-light">GeekBeacon</a></li>
 
-                    <li class="nav-item"><a data-scroll href="/#contact" class="nav-link text-light">Contact</a></li>
+                <li class="nav-item"><a data-scroll href="/#stats" class="nav-link text-light">Stats</a></li>
 
-                    <li class="nav-item"><a href="/" class="nav-link text-light">Home</a></li>
+                <li class="nav-item"><a data-scroll href="/#media" class="nav-link text-light">Instagram Feed</a></li>
+
+                <li class="nav-item"><a data-scroll href="/#geekbeacon" class="nav-link text-light">GeekBeacon</a></li>
+
+                <li class="nav-item"><a data-scroll href="/#contact" class="nav-link text-light">Contact</a></li>
+
+
                 @endif
             </ul>
 
             <hr class="xxs-divider border-white">
 
             @component('components.social')
-                @slot('id', 'socialMobile')
+            @slot('id', 'socialMobile')
             @endcomponent
         </div>
     </nav>
