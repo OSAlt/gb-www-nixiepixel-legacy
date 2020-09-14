ad882978a37fbad122ccc57fffeaf15f5164ce9c
Samir Faci <samir@esamir.com>
2020-09-14 01:40:39 -0700

    Final tweaks before going Life

--- resources/views/components/sections/about.blade.php
+++ resources/views/components/sections/about.blade.php
@@ -6,18 +6,17 @@
         <li class="list-inline-item text-center border border-light p-2 rounded col-4 col-md-2 mb-3 mb-md-0">
             <i class="fa fa-video mb-3 mb-md-4 text-black h2"></i>
             <h4 class="text-blue">Show Host</h4>
-            <p>Affiliate with <a href="https://en.wikipedia.org/wiki/G4_(American_TV_network)">G4TV</a> and TechTV (Discovery Channel)</p>
-        </li>
-        <li class="list-inline-item text-center border border-light p-2 rounded col-4 col-md-2 mb-3 mb-md-0 mr-0 mr-md-1">
-            <i class="fa fa-microphone mb-3 mb-md-4 text-black h2"></i>
-            <h4 class="text-blue">Speaker/Panelist</h4>
-            <p>
+            <p>Show Host for Geekbuzz and OS.ALT on Rev3 (Discovery Channel) Prior G4TV and TechTV Affiliated <br />
+                Speaker/Panelist
                 <ul>
-                    <li><a href="https://gdconf.com/">GDC</a> (Game Developer Conference)</li>
-                    <li> Zurich Game Festival.</li>
-                    <li><a href="https://www.paxsite.com/">Penny Arcade Expo</a><br /></li>
+                    <li>GDC (Game Developer Conference)</li>
+                    <li>Zurich Game Festival</li>
+                    <li>Penny Arcade Expo</li>
+                    <li>GDC 10 Year Media</li>
+                    <li>CES 10 Year Media</li>
+                    <li>E3 8 Year Media</li>
+
                 </ul>
-                
             </p>
         </li>
         <li class="list-inline-item text-center border border-light p-2 rounded col-4 col-md-2">
@@ -37,10 +36,10 @@
             <h4 class="text-blue">Founder / Entrepreneur </h4>
             <p>
                 <ul>
-                    <li> Founded GeekBeacon, a digital community resource and mental health awareness center. <br/></li>
-               <li>Founded Equiti Games, An alternative distribution for games <br/> </li>
-               <li>Founded Geeks Abroad, a geeky travel show <br/></li>
-               <ul>
+                    <li> Founded GeekBeacon, a digital community resource and mental health awareness center. <br /></li>
+                    <li>Founded Equiti Games, An alternative distribution for games <br /> </li>
+                    <li>Founded Geeks Abroad, a geeky travel show <br /></li>
+                    <ul>
             </p>
         </li>
     </ul>
