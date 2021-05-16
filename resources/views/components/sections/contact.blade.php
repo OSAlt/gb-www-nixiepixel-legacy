ea106e7f2900e16d1dfeb8cb58c8555a83f3d42e
csgeek <samir@esamir.com>
2021-05-15 23:11:51 -0700

    Adding blade

--- resources/views/components/sections/contact.blade.php
+++ resources/views/components/sections/contact.blade.php
@@ -1,77 +1,52 @@
 <section id="contact" class="col-12 py-5">
     <h1 class="text-white text-center mb-3">Contact Me!</h1>
-    <form method="GET" action="{{ route('contact') }}" class="form text-light">
-    @csrf
-        <div class="form-row justify-content-around">
-            <div class="mb-4 col-10 col-md-3">
-                <label class="col-form-label-sm" for="name">Name</label>
-                <input class="form-control" type="text" name="name" id="name" placeholder="Name" value="{{old('name')}}" />
 
-                @if ($errors->has('name'))
-                    <span class="invalid-feedback d-block">
-                        <strong>{{ $errors->first('name') }}</strong>
-                    </span>
-                @endif
-            </div>
-            <div class="mb-4 col-10 col-md-3">
-                <label class="col-form-label-sm" for="email">Email</label>
-                <input class="form-control" type="email" name="email" id="email" placeholder="user@domain.tld" value="{{old('email')}}" />
+    <form id="fs-frm" name="department-contact-form" accept-charset="utf-8" action="https://formspree.io/f/xleadqng" method="post">
+        <fieldset id="fs-frm-inputs">
+            <div class="form-row justify-content-around">
+                <div class="mb-4 col-10 col-md-3">
+                    <!-- <label class="col-form-label-sm" for="full-name">Full Name</label> -->
+                    <!-- <input type="text" name="name" id="full-name" placeholder="Name" required=""> -->
+                    <label class="col-form-label-sm" for="name">Name</label>
+                    <input class="form-control" type="text" name="name" id="name" placeholder="Name" value="{{old('name')}}" />
+                </div>
+                <div class="mb-4 col-10 col-md-3">
+                    <!-- <label class="col-form-label-sm" for="email-address">Email Address</label> -->
+                    <!-- <input type="email" style="opacity: 0;" name="_replyto" id="email-address" placeholder="email@domain.tld" required=""> -->
+                    <!-- <input class="form-control" type="email" name="_replyto" id="email-address" placeholder="user@domain.tld" value="" style="opacity: 0;"> -->
+                    <label class="col-form-label-sm" for="email">Email</label>
+                    <input class="form-control" type="email" name="email" id="email" placeholder="user@domain.tld" value="{{old('email')}}" />
 
-                @if ($errors->has('email'))
-                    <span class="invalid-feedback d-block">
-                        <strong>{{ $errors->first('email') }}</strong>
-                    </span>
-                @endif
+                </div>
+                <div class="mb-4 col-10 col-md-3">
+                    <label class="col-form-label-sm" for="subject">Subject</label>
+                    <select class="form-control" name="subject" id="subject">
+                    <!-- <label class="col-form-label-sm" for="subject">Subject</label> -->
+                    <!-- <select name="subject" class="form-control animated zoomIn" id="subject" required="" value="Select"> -->
+                        <option value="Select" disabled="">Select</option>
+                        <option value="Say Hello">Say Hello</option>
+                        <option value="Speaking Engagements">Speaking Engagements</option>
+                        <option value="Business Opportunity">Business Opportunity</option>
+                        <option value="Content Collaboration">Content Collaboration</option>
+                    </select>
+                </div>
             </div>
-            <div class="mb-4 col-10 col-md-3">
-                <label class="col-form-label-sm" for="subject">Subject</label>
-                <select class="form-control" name="subject" id="subject">
-                    <option value="">Choose...</option>
-                    @if($subjects !== NULL)
-                        @foreach($subjects as $subj)
-                            <option value="{{$subj}}">{{$subj}}</option>
-                        @endforeach
-                    @endif
-                </select>
+            <div class="form-row mb-4 justify-content-center">
+                <div class="col-10 col-md-11">
+                    <!-- <label class="col-form-label-sm" for="message">Message</label> -->
+                    <!-- <textarea rows="5" class="form-control animated zoomIn" name="message" id="message" placeholder="Message...." required=""></textarea> -->
+                    <label class="col-form-label-sm" for="message">Message</label>
+                    <textarea class="form-control" name="message" id="message" rows="3" placeholder="Message...">{{old('message')}}</textarea>
 
-                @if ($errors->has('subject'))
-                    <span class="invalid-feedback d-block">
-                        <strong>{{ $errors->first('subject') }}</strong>
-                    </span>
-                @endif
+                </div>
             </div>
-        </div>
-        <div class="form-row mb-4 justify-content-center">
-            <div class="col-10 col-md-11">
-                <label class="col-form-label-sm" for="message">Message</label>
-                <textarea class="form-control" name="message" id="message" rows="3" placeholder="Message...">{{old('message')}}</textarea>
-
-                @if ($errors->has('message'))
-                    <span class="invalid-feedback d-block">
-                        <strong>{{ $errors->first('message') }}</strong>
-                    </span>
-                @endif
+            <div class="text-center">
+                <input type="hidden" name="_subject" id="email-subject" value="NixiePixel Contact Form Submission">
+                <input id="submit" type="submit" value="Submit" class="btn btn-lg btn-success mt-3">
             </div>
-        </div>
-        <div class="text-center">
-            <div class="g-recaptcha row justify-content-center" data-sitekey="{{ config('app.recaptcha_key') }}"></div>
-            @if ($errors->has('g-recaptcha-response'))
-                <span class="invalid-feedback d-block mb-2">
-                    <strong>Please check the box to confirm you are not a bot.</strong>
-                </span>
-            @endif
-            <input id="submit" type="submit" value="Submit" class="btn btn-lg btn-success mt-3">
-        </div>
+        </fieldset>
     </form>
 
-    {{--<a href="https://www.patreon.com/bePatron?u=193956" data-patreon-widget-type="become-patron-button">Become a Patron!</a><script async src="https://c6.patreon.com/becomePatronButton.bundle.js"></script>--}}
 
-    {{--<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">--}}
-        {{--<input type="hidden" name="cmd" value="_donations" />--}}
-        {{--<input type="hidden" name="business" value="sendlove@nixiepixel.com" />--}}
-        {{--<input type="hidden" name="currency_code" value="USD" />--}}
-        {{--<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />--}}
-        {{--<img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />--}}
-    {{--</form>--}}
 
 </section>
