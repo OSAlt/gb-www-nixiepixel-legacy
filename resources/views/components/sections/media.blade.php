5b1defd5bdacec27e47f867372e8ebaa2920fcdf
Samir Faci <samir@esamir.com>
2022-03-24 16:50:02 -0400

    Adding social media

--- resources/views/components/sections/media.blade.php
+++ resources/views/components/sections/media.blade.php
@@ -1,5 +1,17 @@
 <section id="media" class="pb-4 pt-5">
-    <h1 class="text-white text-center mt-3 mb-4">Checkout My Insta Love!</h1>
+<!-- Place <div> tag where you want the feed to appear -->
+<div id="curator-feed-default-feed-layout"><a href="https://curator.io" target="_blank" class="crt-logo crt-tag">Powered by Curator.io</a></div><!-- The Javascript can be moved to the end of the html page before the </body> tag --><script type="text/javascript">
+/* curator-feed-default-feed-layout */
+(function(){
+var i,e,d=document,s="script";i=d.createElement("script");i.async=1;i.charset="UTF-8";
+i.src="https://cdn.curator.io/published/665652c2-9507-4961-9372-637c95380d14.js";
+e=d.getElementsByTagName(s)[0];e.parentNode.insertBefore(i, e);
+})();
+</script>
+
+
+    <!-- <h1 class="text-white text-center mt-3 mb-4">Checkout My Insta Love!</h1>
+
     <ul class="list-unstyled row justify-content-center justify-content-md-around text-light">
         @if($media !== NULL)
             @foreach($media as $item)
@@ -29,5 +41,5 @@
                 </a>
             </li>
         @endif
-    </ul>
+    </ul> -->
 </section>
